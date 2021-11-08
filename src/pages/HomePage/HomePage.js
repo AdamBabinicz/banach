7f538ca6002bb5e2ea7907c3806d3c44ce96fd40
babinicz <babinicz@onet.pl>
2021-11-08 16:26:40 +0100

    dodałem popup

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Hero from "../../components/Hero/Hero";
 import Content from "../../components/Content/Content";
 import Pricing from "../../components/Pricing/Pricing";
 import { heroOne, heroTwo, heroThree } from "../../data/homeData";
+import Popup from "../../components/Popup/Popup";
 
 const HomePage = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const [buttonPopup, setButtonPopup] = useState(false);
+  const [timedPopup, setTimedPopup] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setTimedPopup(true);
+    }, 2000);
+  }, []);
+
   return (
     <>
       <Hero />
@@ -12,6 +26,33 @@ const HomePage = () => {
       <Content {...heroTwo} />
       <Content {...heroThree} />
       <Pricing />
+      <Popup trigger={timedPopup} setTrigger={setTimedPopup}>
+        <h3
+          style={{
+            marginBottom: "2.5rem",
+            textAlign: "center",
+            lineHeight: "3.5rem",
+          }}
+        >
+          Stefan Banach - karmiciel wszy!
+          <span aria-label="Description of the overall image" role="img">
+            &nbsp;
+          </span>
+        </h3>
+        <p>
+          Profesor Rudolf Weigl pomógł ochronić wielu usuniętych z pracy
+          profesorów uniwersytetu i ich współpracowników, zatrudniając ich jako
+          karmicieli wszy. Takie zatrudnienie uprawniało do specjalnych racji
+          żywnościowych i - chociaż częściowo - ubezpieczało przed
+          aresztowaniami, wywózką i/lub śmiercią pod okupacją hitlerowską.
+          Niektóre aspekty zatrudnienia w Instytucie Weigla miały wiele
+          wspólnego z filmem Spielberga „Lista Schindlera”. <br />
+          <br />
+          <em>www.lwow.com.pl/m.htm</em>
+          <br />
+          <br />
+        </p>
+      </Popup>
     </>
   );
 };
